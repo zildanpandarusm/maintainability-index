118fef0fc1826298ee3dfa0ff2491edaa058c95d
zildanpandarusm <zildanmarginata@gmail.com>
2025-04-26 00:53:55 +0700

    forth commit

--- backend/src/routes/maintainability-index.route.js
+++ backend/src/routes/maintainability-index.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import { analyzeCodeFromZip, detectDataClassSmell, detectParallelInheritanceHierarchies, exportCsv } from '../../controllers/maintainability-index.controller.js';
+import { analyzeCodeFromZip, detectDataClassSmell, detectParallelInheritanceHierarchies, exportCsv } from '../controllers/maintainability-index.controller.js';
 
 const router = express.Router();
 const storage = multer.memoryStorage();
