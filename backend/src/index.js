9fefcae1096d579a4a839f9c92ac6819a6d727d8
zildanpandarusm <zildanmarginata@gmail.com>
2025-04-29 01:02:20 +0700

    fix cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,11 +12,8 @@ app.use(
   })
 );
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/maintainability', maintainabilityRoutes);
