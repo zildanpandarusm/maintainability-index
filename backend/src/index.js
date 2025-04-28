ddc2c346c64e976c44f94624d306090c80e676ed
zildanpandarusm <zildanmarginata@gmail.com>
2025-04-29 00:45:00 +0700

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,11 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routes
 app.use('/api/maintainability', maintainabilityRoutes);
